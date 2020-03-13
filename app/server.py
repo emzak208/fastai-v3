4d61ac460dc1668065dbf25c9c4db565fcb65b30
mihap <emzak208@users.noreply.github.com>
2020-03-13 10:51:18 -0700

    modified link and classes

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://www.googleapis.com/drive/v3/files/1TQg0VvEBS5f-ibOcMg_G-jIzKZUyvYoK?alt=media&key=AIzaSyDQ61NB0kspwJbQjVAbwax-IIoHFWsM4do'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['monet', 'munch', 'picasso']
 path = Path(__file__).parent
 
 app = Starlette()
